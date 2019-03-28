c2e5ea967efec0d413af6d0c21671126b333d4b1
zeitmeister <simon.sporrong@hotmail.se>
2019-03-28 21:38:06 +0100

    Updated detail action method to use action result type

--- src/ComicBookGallery/Controllers/ComicBooksController.cs
+++ src/ComicBookGallery/Controllers/ComicBooksController.cs
@@ -8,9 +8,13 @@ namespace ComicBookGallery.Controllers
 {
     public class ComicBooksController : Controller
     {
-        public string Detail()
+        public ActionResult Detail()
         {
-            return "Hello from the comic books controller";
+            if(DateTime.Today.DayOfWeek == DayOfWeek.Friday)
+            {
+                return Redirect("/");
+            }
+            return Content("Hello from the comic books controller");
         }
     }
 }
---
+++ src/ComicBookGallery/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ComicBookGallery.Controllers
+{
+    public class HomeController : Controller
+    {
+        public string Index()
+        {
+            return "Simpa är snygg tycker jag!";
+        }
+    }
+}
