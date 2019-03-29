d60cafbc43599c1a9b6c4d3dfbcb6eddc1f554fc
zeitmeister <simon.sporrong@hotmail.se>
2019-03-29 23:04:23 +0100

    Added Comic Book Detail view

--- src/ComicBookGallery/Controllers/ComicBooksController.cs
+++ src/ComicBookGallery/Controllers/ComicBooksController.cs
@@ -10,11 +10,7 @@ namespace ComicBookGallery.Controllers
     {
         public ActionResult Detail()
         {
-            if(DateTime.Today.DayOfWeek == DayOfWeek.Friday)
-            {
-                return Redirect("/");
-            }
-            return Content("Hello from the comic books controller");
+            return View();
         }
     }
 }
